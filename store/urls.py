67ddcedc50464faa8eb2cbf7e268ca2ad2185a14
Azam <abdurazakovazam01@gmail.com>
2023-03-12 10:19:30 +0500

    Rename views, changed page titles and template names

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from store.views import all_products, category_list, product_detail
+from store.views import product_all, category_list, product_detail
 
 app_name = 'store'
 
 urlpatterns = [
-    path('', all_products, name='all_products'),
+    path('', product_all, name='product_all'),
     path('item/<slug:slug>/', product_detail, name='product_detail'),
     path('search/<slug:category_slug>/', category_list, name='category_list'),
 ]
