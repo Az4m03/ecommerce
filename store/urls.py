43c13b12149511445c0a9404df3c20081f4813a5
Azam <abdurazakovazam01@gmail.com>
2023-03-12 10:30:41 +0500

    Changed the URL structures

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ app_name = 'store'
 
 urlpatterns = [
     path('', product_all, name='product_all'),
-    path('item/<slug:slug>/', product_detail, name='product_detail'),
+    path('<slug:slug>', product_detail, name='product_detail'),
     path('search/<slug:category_slug>/', category_list, name='category_list'),
 ]
