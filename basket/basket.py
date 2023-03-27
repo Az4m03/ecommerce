d16e8dfd5a2505c1e429438b1e3cf2c90cce8686
Azam <abdurazakovazam01@gmail.com>
2023-03-27 17:33:21 +0500

    Added the Qty to the session data

--- basket/basket.py
+++ basket/basket.py
@@ -12,9 +12,16 @@ class Basket():
             basket = self.session['skey'] = {}
         self.basket = basket
 
-    def add(self, product):
+    def add(self, product, qty):
         product_id = product.id
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price)}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
 
         self.session.modified = True
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
